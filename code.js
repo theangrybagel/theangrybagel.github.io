f57d3bc0236f6465108f8e477fcb7c66749a05da
theangrybagel <therealangrybagel@gmail.com>
2020-07-25 10:24:01 -0700

    Adding a new expansion to the lore
    Added omanian lore.

--- code.js
+++ code.js
@@ -1,4 +1,32 @@
 setTimeout(function(){AddNavbar();}, 10);
+setTimeout(function(){ApplySpoilers();}, 100);
+//setInterval(SpoilerUpdate, 10)
+
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
+var spoilerData = {};
+var currentlyTargetedSpoilers = [];
+/*
+function SpoilerUpdate()
+{
+    currentlyTargetedSpoilers.forEach(function(e)
+    {
+        indexOfChosenIndex = rand(0, e.indexes.length);
+        chosenIndex = e.indexes[indexOfChosenIndex];
+        newTxt = e.spoiler.innerHTML;
+        var changeToReal = rand(0, 10) < 9;
+        if(changeToReal)
+        {
+            newTxt = newTxt.replaceAt(chosenIndex, e.targetTxt[chosenIndex]);
+            e.indexes.splice(indexOfChosenIndex, 1);
+        }
+        else{
+
+        }
+        e.spoiler.innerHTML = newTxt;
+    });
+}*/
 function getE(elementName)
 {
     return document.getElementById(elementName);
@@ -22,6 +50,75 @@ var element = document.getElementsByTagName('head')[0];
 element.appendChild(para);
 }
 
+function RandLetter()
+{
+    arr = "abcdefghijklmnopqrstuvwxyz1234567890"
+    return arr[rand(0, arr.length)]
+}
+
+function RevealSpoiler(id)
+{
+    //console.location("Reveal " + id);
+    var spoiler = document.getElementById(id);
+    if(spoiler == null) return;
+    var targetTxt = spoilerData[id];
+    var indexes = [];
+    spoiler.innerHTML = targetTxt;
+    for(var i = 0; i < targetTxt.length; i+=1)
+    {
+        indexes.push(i);
+    }
+    newSpoiler = {spoiler: spoiler, targetTxt: targetTxt, indexes: indexes}
+    currentlyTargetedSpoilers.push(newSpoiler);
+    /*
+    while(indexes.length > 0)
+    {
+
+        
+        console.log(newTxt);
+    }*/
+}
+function HideSpoiler(id)
+{
+    var spoiler = document.getElementById(id);
+    if(spoiler == null) return;
+    n = "";
+
+    var inTag = false
+    spoiler.innerHTML.split("").forEach(function(letter)
+    {
+        if(letter == "<")
+            inTag = true;
+        if(letter == ">")
+            inTag = false;
+        if([" ", "<", ">", ",", "."].includes(letter) || inTag)
+        {
+            n += letter;
+        }
+        else{
+            n += RandLetter();
+        }
+
+    });
+    spoiler.innerHTML = n;
+}
+function ApplySpoilers()
+{
+    spoilers = document.getElementsByClassName("spoilers");
+    document.querySelectorAll('.edit').forEach(function(button) {
+    // Now do something with my button
+});
+    var index = -1;
+    document.querySelectorAll(".spoilers").forEach(function(spoiler){
+        index += 1;
+        console.log(spoiler)
+        spoiler.id = "spoiler" + index;
+        spoilerData[spoiler.id] = spoiler.innerHTML;
+        spoiler.setAttribute("onmouseenter", "RevealSpoiler('spoiler" + index+"')")
+        spoiler.setAttribute("onmouseleave", "HideSpoiler('spoiler" + index+"')")
+        HideSpoiler(spoiler.id);
+    });
+}
 function Search() {
         var result = "";
         var searchTxt = getE("search00").value.toLowerCase();
